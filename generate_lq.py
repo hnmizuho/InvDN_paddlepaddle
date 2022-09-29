e9b90a595a3852024c2857fa6c9a1a5d6f26ef44
CC <yanyan1999421@gmail.com>
2022-09-29 15:44:53 +0800

    修正了生成lq时文件夹不存在的问题

--- generate_lq.py
+++ generate_lq.py
@@ -8,19 +8,32 @@ from networks.InvDN_model import InvNet, constructor, gaussian_batch
 from utils import dir_utils,img_utils
 import scipy
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description="generate lq")
+parser.add_argument("--data_dir", type=str, default="./SIDD_Medium_Srgb_Patches_512/train/", help="path of train dataset")
+parser.add_argument("--val_dir", type=str, default="./SIDD_Valid_Srgb_Patches_256/valid/", help="path of val dataset")
+parser.add_argument("--scale", type=int, default=4, help="scale")
+parser.add_argument("--gt_size", type=int, default=144, help="crop size for training")
+opt = parser.parse_args()
 
 def run_one_epoch():
-    opt = Config('training_4cards.yml') 
+    alternative_opt = {
+        "train_dir":opt.data_dir,
+        "val_dir":opt.val_dir,
+        "scale":opt.scale,
+        "gt_size":opt.gt_size
+    }
 
-    train_dataset = LQGTRNDataset(opt, is_train=True)
+    train_dataset = LQGTRNDataset(opt=None, is_train=True, alternative_opt=alternative_opt)
     train_loader = DataLoader(dataset=train_dataset, batch_size=1, shuffle=False, num_workers=1, drop_last=False)
-
-    val_dataset = LQGTRNDataset(opt, is_train=False)
-    val_loader = DataLoader(dataset=val_dataset, batch_size=1, shuffle=False, num_workers=1, drop_last=False)
 
     with paddle.no_grad():
         for iter,data_val in enumerate(train_loader):
-            print(iter)
+            print("lq image ", iter, " done.")
+            break
+    
+    print("All LQ images generated.")
 
 if __name__ == '__main__':
     run_one_epoch()
